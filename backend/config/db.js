9815161b819e0f4711de980b61dfb67967e43254
Ravindu-Dulanjana <krdulanjana@gmail.com>
2023-03-06 15:40:52 +0530

    backend complete crud operations

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDb Connect : ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(err);
+    console.log(error);
     process.exit(1);
   }
 };
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { registerUser, login, getMe } = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
+
+router.post("/", registerUser);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+
+module.exports = router;
